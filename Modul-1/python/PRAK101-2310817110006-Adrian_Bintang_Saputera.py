f3ff15dc668411cd8a13e6fe743969de77766c0f
Adrian Bintang Saputera <129594599+MadeByBintang@users.noreply.github.com>
2023-09-26 17:41:05 +0700

    Delete Modul-1 directory

--- Modul-1/python/PRAK101-2310817110006-Adrian_Bintang_Saputera.py
+++
@@ -1,5 +0,0 @@
-x = int(input("Variabel x bernilai: "))
-y = int(input("Variabel y bernilai: "))
-z = int(input("Variabel z bernilai: "))
-
-print("Jumlah variabel tersebut adalah", x + y + z)
--- Modul-1/python/PRAK102-2310817110006-Adrian_Bintang_Saputera.py
+++
@@ -1,6 +0,0 @@
-a = float(input("Vaiabel a bernilai: "))
-b = float(input("Vaiabel b bernilai: "))
-c = float(input("Vaiabel c bernilai: "))
-
-hasil = a * b / c
-print(f"Hasil dari a dikali b dibagi c adalah {hasil:.6f}")
